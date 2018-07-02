c06be8c184bbac3b64a76a1cbc5939252a35c3d0
Luke2691 <l.ussi@campus.unimib.it>
2018-07-02 17:40:45 +0200

    Update mongoread.py and added mongoread2.py

--- mongoread.py
+++ mongoread.py
@@ -4,74 +4,81 @@ import json
 import string
 import sys
 import re
+import collections
+import bson
+from bson.codec_options import CodecOptions
+from pyshark_parser import packet_util
+from pyshark_parser import layer_util
 
 
 def main():
     cap = pyshark.FileCapture(sys.argv[1])
-    cont = 0
-    indexvalue = 0
-    indextype = 0
+    #cont = 0
     for packet in cap:
-        if 'MONGO' in str(packet.layers):
-            if packet.mongo.element_name=="cursor": #insert, update per le request
-                #dir restituisce l'elenco degli attributi dell'oggetto che viene passato come argomento.
-                print("---------------------mongo packet------------------------------------")
-                packetcontent=str(packet.mongo);
-                print(packetcontent)
-                line = packetcontent.split('\n')
-                lung = len(line) - 1
-                for i in range(0,lung):
-                    #Leggo linea per linea
-                    print("linea:"+line[i]+"\n")
-                    #Elimino tutti gli spazi
-                    value=line[i].strip()
-                    value=value.replace(" ","")
-                    content=value.split(":")
-                    print(content)
-                    #if(len(content
-                    #  )==2)
+        #print("-----------------prova---------------------")
+        #Prende tutti i campi del pacchetto mongo
+        if not packet:
+            print(None)
+        layer = 'mongo'
+        field_names = set()
+        #prendo tutti i layer (protocolli) presenti nel pacchetto
+        for current_layer in packet.layers:
+            #print("current: "+str(current_layer))
+            #print("current: "+current_layer.__dict__['_layer_name'])
+            #controllo se layer considerato è MONGO
+            if not layer or layer == current_layer.__dict__['_layer_name']:
+                #print("layer: "+str(layer))
+                #print("elemento current:" + str(current_layer))
+                #Tutti i campi di questo livello
+                fields = current_layer.__dict__['_all_fields']
+                print(fields)
+                for field in fields:
+                    field_names.add(field)
+                    #print("elemento field:" + fields[field])
+                    # if fields["mongo.message_length"] == "194":
+                    #     print("elemento field:" + str(field))
+                    #Prendo in considerazione il campo "Query"
+                    # if "Query" in fields[field]:
+                    #     #print("field_:"+str(type(fields[field])))
+                    #     fieldquery = fields[field].__dict__['fields']
+                    #     #print(type(fieldquery))
+                    #     for p in fieldquery:
+                    #         print("elemento lista:"+str(dir(p)))
+                    #         #print("elemento lista:" + str(p.__getattribute__['Elements']))
+                    #         for k in iter(p.__getattribute__):
+                    #             print(str(k))
 
-                    # print("---------------------attributi mongo packet getitem------------------------------------")
-                    # print(type(packetcontent))
-                    # print(type(packetcontent.element_name))
-                    # print(packetcontent.element_name.size)
-                    # print(packetcontent.document_length)
-                    # print(packetcontent.__getattribute__("Document"))
-                    print("---------------------mongo packet fields------------------------------------")
-                    fieldnames = packet.mongo.field_names
-                    print(fieldnames)
-                    print("---------------------mongo packet document-----------------------------------")
-                    print(packet.mongo.document)
-                    print("---------------------mongo packet opcode------------------------------------")
-                    print(packet.mongo.opcode)
-                    print("---------------------mongo packet raw_mode------------------------------------")
-                    print(packet.mongo.raw_mode)
-                    print("---------------------attributi di elements------------------------------------")
-                    print(dir(packet.mongo.elements))
-                    print("---------------------tutti i campo di elements------------------------------------")
-                    print(packet.mongo.elements.all_fields)
-                    # print("---------------------TCP------------------------------------")
-                    # print(dir(cap[cont].tcp))
-                    # print("--------------------TCP PAYLOAD-------------------------------------")
-                    # print(dir(cap[cont].tcp.payload))
-                    # print("---------------------MONGO------------------------------------")
-                    # print(dir(cap[cont].mongo))
-                    # print("--------------------MONGO Response-------------------------------------")
-                    # print(dir(cap[cont].mongo.response_to))
-                    # print("--------------------Content-------------------------------------")
-                    # print(cap[cont].mongo.response_to.decode("ascii", "ignore"))
-                    # print("--------------------MONGO Query-------------------------------------")
-                    # print(get(cap[cont].mongo,"query",None))
-                    # print("--------------------Content-------------------------------------")
-                    # print(cap[cont].mongo.query)
-                    # print("--------------------MONGO __getitem__--------------------------------")
-                    # print(dir(cap[cont].mongo.query.__getitem__(1)))
 
-                    # if value.startswith("Value:"):
-                    #      print("valore letto "+value)
-                print("---------------------attributi mongo packet------------------------------------")
-                print(dir(packet.mongo))
-        cont = cont+1
+                        #print(fields[field].__dict__['fields'])
+                        #print(dir(fields[field]))
+        # layer = 'json'
+        # field_names = set()
+        # for current_layer in packet.layers:
+        #     # print("current: "+str(current_layer))
+        #     # print("current: "+current_layer.__dict__['_layer_name'])
+        #     if not layer or layer == current_layer.__dict__['_layer_name']:
+        #         print("layer: " + str(layer))
+        #         for field in current_layer.__dict__['_all_fields']:
+        #             field_names.add(field)
+        #             print("field_json:" + current_layer.__dict__['_all_fields'][field])
+        #print(field_names)
+        #print("---------------fine prova ----------------------")
+
+
+
+        # if 'MONGO' in str(packet.layers):
+        #     if packet.mongo.element_name=="cursor": #insert, update per le request
+        #         #dir restituisce l'elenco degli attributi dell'oggetto che viene passato come argomento.
+        #         print("---------------------mongo packet------------------------------------")
+        #         packetcontent=packet.mongo
+        #         print(packetcontent)
+        #         print("---------------------attributi mongo packet------------------------------------")
+        #         print(dir(packetcontent))
+        #         print("---------------------mongo packet data------------------------------------")
+        #         print(packetcontent.document)
+        #         print(type(packetcontent.document))
+        #
+        # cont = cont+1
 
 if __name__ == "__main__":
     main()
